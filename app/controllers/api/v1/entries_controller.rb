455df48365ee735129c53167f6778efe4c351130
Nathan Silva Santos <nathan@w16.com.br>
2023-03-16 03:54:10 -0300

    Feat: Entries | Pagination, Search, Order By and Direction.

--- app/controllers/api/v1/entries_controller.rb
+++ app/controllers/api/v1/entries_controller.rb
@@ -3,9 +3,48 @@ class Api::V1::EntriesController < ApplicationController
 
   # GET /entries
   def index
-    @entries = Entry.order(date: :asc).limit(20)
+    @direction = "ASC"
+    if params[:direction]
+      @direction = params[:direction]
+    end
 
-    render json: { data: @entries }
+    @order_by = "id"
+    if params[:order_by]
+      @order_by = params[:order_by]
+    end
+
+    @page = 1
+    if params[:page]
+      @page = params[:page].to_i
+    end
+
+    @per_page = 25
+    if params[:per_page]
+      @per_page = params[:per_page].to_i
+    end
+
+    @search = params[:search]
+
+    @result = Entry.order(@order_by + " " + @direction).page(@page).per(@per_page)
+
+    if @search
+      @result = @result.search_by_term(@search)
+    end
+
+    @total = @result.total_count
+
+    @last_page = @total.fdiv(@per_page).ceil
+
+    render json: {
+      result: @result,
+      direction: @direction,
+      order_by: @order_by,
+      page: @page,
+      per_page: @per_page,
+      search: @search,
+      total: @total,
+      last_page: @last_page,
+    }
   end
 
   # GET /entries/1
--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,7 +1,17 @@
 class Entry < ApplicationRecord
   include OrderableByTimestamp
+  include PgSearch::Model
 
   validates :name, presence: true
   validates :date, presence: true
   validates :value, presence: true
+
+  pg_search_scope :search_by_term,
+    against: :name,
+    using: {
+      tsearch: {
+        any_word: true,
+        prefix: true,
+      },
+    }
 end
