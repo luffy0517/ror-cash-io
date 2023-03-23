871e9b7a74b220d00d7806452cbc61dab98b19f4
luffyui <tanaka.yuki000@hotmail.com>
2023-03-22 23:31:55 -0300

    Feat: Add Docker

---
+++ db/seeds/development.rb
@@ -0,0 +1,10 @@
+1800.times do
+  Entry.create!({
+                  category_id: Faker::Number.between(from: 1, to: 7),
+                  user_id: 1,
+                  name: Faker::Name.name,
+                  description: Faker::Lorem.paragraph,
+                  date: Faker::Date.between(from: 180.days.ago, to: Date.today),
+                  value: Faker::Number.between(from: -1000, to: 1000)
+                })
+end
